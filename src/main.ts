7a2cf3751c77909809e8678b3dc3bde64935a009
assynour <assy.n.nour@gmail.com>
2023-03-10 10:50:10 +0100

    display main bpmn diagram

--- src/main.ts
+++ src/main.ts
@@ -14,35 +14,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import './style.css';
 import {BpmnVisualization, FitType} from 'bpmn-visualization';
-// This is simple example of the BPMN diagram, loaded as string. The '?.raw' extension support is provided by Vite.
-// For other load methods, see https://github.com/process-analytics/bpmn-visualization-examples
-// eslint-disable-next-line n/file-extension-in-import -- Vite syntax
-import diagram from './diagram.bpmn?raw';
 
-// Instantiate BpmnVisualization, pass the container HTMLElement - present in index.html
-const bpmnVisualization = new BpmnVisualization({
-  container: 'bpmn-container',
-  navigation: {
-    enabled: true,
-  },
+import collapsedDiagram from './diagrams/EC-purchase-orders-collapsed.bpmn?raw';
+
+// 'bpmn-visualization' API documentation: https://process-analytics.github.io/bpmn-visualization-js/api/index.html
+const mainBpmnVisualization = new BpmnVisualization({
+  container: 'main-bpmn-container',
 });
-// Load the BPMN diagram defined above
-bpmnVisualization.load(diagram, {fit: {type: FitType.Center, margin: 20}});
 
-// Highlight task
-bpmnVisualization.bpmnElementsRegistry.addCssClasses(
-  [
-    'Activity_00vbm9s', // Record Goods Receipts
-    'Activity_1t65hvk', // Create Purchase Order Item
-  ],
-  'bpmn-highlight',
-);
-
-// Display the bpmn-visualization version in the footer
-const footer = document.querySelector<HTMLElement>('footer')!;
-const version = bpmnVisualization.getVersion();
-const versionAsString = `bpmn-visualization@${version.lib}`;
-const dependenciesAsString = [...version.dependencies].map(([name, version]) => `${name}@${version}`).join('/');
-footer.textContent = `${versionAsString} with ${dependenciesAsString}`;
+// Load BPMN diagram
+// Try the "Center" type to fit the whole container and center the diagram
+mainBpmnVisualization.load(collapsedDiagram, {fit: {type: FitType.Center, margin: 20}});
