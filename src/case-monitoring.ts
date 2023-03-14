746fc204c0f448f22da43f202b7947d45842c81b
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-03-14 07:14:15 +0100

    case-monitoring.ts add TODO

--- src/case-monitoring.ts
+++ src/case-monitoring.ts
@@ -34,6 +34,7 @@ function reduceVisibilityOfAlreadyExecutedElements(bpmnVisualization: BpmnVisual
 }
 
 function restoreVisibilityOfAlreadyExecutedElements(bpmnVisualization: BpmnVisualization) {
+  // TODO why adding pending?  the CSS class was not added in reduceVisibilityOfAlreadyExecutedElements
   bpmnVisualization.bpmnElementsRegistry.removeCssClasses(Array.from([...caseMonitoringData.executedShapes, ...caseMonitoringData.pendingShapes, ...caseMonitoringData.visitedEdges]), 'state-already-executed');
 }
 
