f96acf70d6ba93d0c028eca84a6ac12f7c0ab50e
assynour <assy.n.nour@gmail.com>
2023-03-16 16:24:18 +0100

    add popover on retrieve email activity

--- src/case-monitoring.ts
+++ src/case-monitoring.ts
@@ -4,6 +4,7 @@ import type {BpmnElement, BpmnSemantic, BpmnVisualization} from 'bpmn-visualizat
 import {getActivityRecommendationData} from './recommendation-data.js';
 import {type CaseMonitoringData, fetchCaseMonitoringData} from './case-monitoring-data.js';
 import {displayView, ProcessVisualizer, subProcessBpmnVisualization, subProcessViewName} from './diagram.js';
+import {BpmnElementsSearcher} from './utils/bpmn-elements.js';
 
 abstract class AbstractCaseMonitoring {
   protected caseMonitoringData: CaseMonitoringData | undefined;
@@ -125,6 +126,25 @@ class SubProcessCaseMonitoring extends AbstractCaseMonitoring {
   }
 }
 
+class MainProcessSupplierPoolCaseMonitoring extends AbstractCaseMonitoring {
+  constructor(bpmnVisualization: BpmnVisualization) {
+    super(bpmnVisualization, 'main');
+  }
+
+  getTippySupportInstance() {
+    return this.tippySupport;
+  }
+
+  addInfoOnChatGptActivity(bpmnElementId: string) {
+    const tippyInstance = this.tippySupport.addPopover(bpmnElementId);
+    return tippyInstance;
+  }
+
+  protected createTippySupportInstance(bpmnVisualization: BpmnVisualization): AbstractTippySupport {
+    return new MainProcessSupplierPoolTippySupport(bpmnVisualization);
+  }
+}
+
 // May change in the future to favor composition over inheritance.
 abstract class AbstractTippySupport {
   protected registeredBpmnElements = new Map<Element, BpmnSemantic>();
@@ -160,6 +180,7 @@ abstract class AbstractTippySupport {
     } as Partial<Props>);
 
     this.tippyInstances.push(tippyInstance);
+    return tippyInstance;
   }
 
   removeAllPopovers(): void {
@@ -315,6 +336,57 @@ class SubProcessTippySupport extends AbstractTippySupport {
   }
 }
 
+class MainProcessSupplierPoolTippySupport extends AbstractTippySupport {
+  protected userQuestion: string | undefined = 'Write a short email to ask the supplier about the delay';
+  protected chatGptAnswer: string | undefined = 'Generating email template';
+
+  constructor(protected readonly bpmnVisualization: BpmnVisualization) {
+    super(bpmnVisualization);
+  }
+
+  setUserQuestion(userQuestion: string) {
+    this.userQuestion = userQuestion;
+  }
+
+  setChatGptAnswer(chatGptAnswer: string) {
+    this.chatGptAnswer = chatGptAnswer;
+  }
+
+  protected getContent(htmlElement: ReferenceElement) {
+    return this.getEmailTemplateContent(htmlElement);
+  }
+
+  protected registerEventListeners(instance: Instance): void {
+    // TODO: Implement this method
+  }
+
+  private getEmailTemplateContent(htmlElement: ReferenceElement) {
+    const popoverContent = `
+        <div class="popover-container">
+          <h5>Retrieving an email template</h5>
+          <table>
+            <tbody>
+              <tr class="popover-row">
+                <td class="popover-key">You</td>
+                <td class="popover-value"><textarea>${this.userQuestion!}</textarea>
+              </tr>
+              <tr class="popover-row">
+                <td class="popover-key">Chat GPT</td>
+                <td class="popover-key"><textarea>${this.chatGptAnswer!}</textarea>
+              </tr>
+            </tbody>
+          </table>
+        </div>`;
+
+    const bpmnSemantic = this.registeredBpmnElements.get(htmlElement);
+    if (bpmnSemantic?.name === 'Review and adapt email') {
+      // Add validate and abort buttons to the popover
+    }
+
+    return popoverContent;
+  }
+}
+
 // eslint-disable-next-line no-warning-comments -- cannot be managed now
 // TODO used by subprocess
 function getWarningInfoAsHtml() {
@@ -390,14 +462,32 @@ async function showContactClientAction(bpmnVisualization: BpmnVisualization) {
   const processVisualizer = new ProcessVisualizer(bpmnVisualization);
   processVisualizer.showManuallyTriggeredProcess();
 
+  const supplierMonitoring = new MainProcessSupplierPoolCaseMonitoring(bpmnVisualization);
+  const tippySupportInstance = supplierMonitoring.getTippySupportInstance() as MainProcessSupplierPoolTippySupport;
+  // Add popover to "retrieve email suggestion"
+  const retrieveEmailActivityId = new BpmnElementsSearcher(bpmnVisualization).getElementIdByName('Retrieve email suggestion');
+  if (retrieveEmailActivityId !== undefined) {
+    if (tippySupportInstance !== undefined) {
+      tippySupportInstance.setUserQuestion('Write a short email to ask the supplier xyz about the delay in the approval and the expected new arrival date');
+      tippySupportInstance.setChatGptAnswer('Generating an email template...');
+    }
+
+    const tippyInstance = supplierMonitoring.addInfoOnChatGptActivity(retrieveEmailActivityId);
+    tippyInstance.setProps({
+      trigger: 'manual',
+      arrow: false,
+    });
+    tippyInstance.show();
+  }
+
   // Hide pool when the process execution terminates
   // Wait for 5 seconds for simulation
-  await new Promise<void>(resolve => {
+  /* await new Promise<void>(resolve => {
     setTimeout(() => {
       resolve();
     }, 5000);
   });
 
-  processVisualizer.hideManuallyTriggeredProcess();
+  processVisualizer.hideManuallyTriggeredProcess(); */
 }
 
