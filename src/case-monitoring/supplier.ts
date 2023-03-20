192918a9db4dd2f7e1ec389e2cc3a990800cff0a
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-03-20 14:49:28 +0100

    feat(case-monitoring): progressively highlight path of supplier (#26)
    Highlight the first sub-path when starting the process (from start to )
    Then, highlight the "review and adapt email" activity and decrease the
    opacity of the first sub-path.

--- src/case-monitoring/supplier.ts
+++ src/case-monitoring/supplier.ts
@@ -17,6 +17,48 @@ class SupplierProcessCaseMonitoring extends AbstractCaseMonitoring {
     return this.tippySupport.addPopover(bpmnElementId);
   }
 
+  highlightFirstElementsOnStart(): void {
+    // Event_1t5st9j start event
+    // Flow_0i8gykc
+    // Gateway_19radi6
+    // Flow_06y94ol
+    // Activity_04d6t36 chatGPT activity
+    this.bpmnVisualization.bpmnElementsRegistry.updateStyle(['Event_1t5st9j', 'Gateway_19radi6', 'Activity_04d6t36'], {
+      stroke: {color: 'blue', width: 4},
+      fill: {color: 'blue', opacity: 10},
+    });
+    this.bpmnVisualization.bpmnElementsRegistry.updateStyle(['Flow_0i8gykc', 'Flow_06y94ol'], {
+      stroke: {color: 'blue', width: 4},
+    });
+  }
+
+  highlightReviewEmail(): void {
+    this.bpmnVisualization.bpmnElementsRegistry.updateStyle(['Activity_1oxewnq'], {
+      stroke: {color: 'blue', width: 3},
+      fill: {color: 'blue', opacity: 10},
+    });
+    this.bpmnVisualization.bpmnElementsRegistry.updateStyle(['Flow_092it75'], {
+      stroke: {color: 'blue', width: 4},
+    });
+    // Reduce opacity of previous elements
+    // Temp implementation: the list should not be duplicated with the `highlightFirstElementsOnStart` method
+    this.bpmnVisualization.bpmnElementsRegistry.updateStyle(
+      [
+        'Event_1t5st9j',
+        'Gateway_19radi6',
+        'Activity_04d6t36', // Shapes
+        'Flow_0i8gykc',
+        'Flow_06y94ol', // Edges
+      ],
+      {
+        opacity: 5,
+        font: {
+          opacity: 15, // The global opacity doesn't affect the font opacity, so we must redefine it here :-(
+        },
+      },
+    );
+  }
+
   protected createTippySupportInstance(bpmnVisualization: BpmnVisualization): AbstractTippySupport {
     return new SupplierProcessTippySupport(bpmnVisualization);
   }
@@ -85,11 +127,15 @@ class SupplierProcessTippySupport extends AbstractTippySupport {
 class SupplierContact {
   constructor(readonly bpmnVisualization: BpmnVisualization, readonly supplierMonitoring: SupplierProcessCaseMonitoring) {}
 
+  // eslint-disable-next-line no-warning-comments -- cannot be managed now
+  // TODO this could should really be async!!!
   async startInstance(): Promise<void> {
     let prompt = '';
     let answer = '';
     let emailRetrievalTippyInstance;
     let emailReviewTippyInstance;
+
+    this.supplierMonitoring.highlightFirstElementsOnStart();
 
     // Add and show popover to "retrieve email suggestion"
     const retrieveEmailActivityId = new BpmnElementsSearcher(this.bpmnVisualization).getElementIdByName('Retrieve email suggestion');
@@ -117,6 +163,8 @@ class SupplierContact {
       emailReviewTippyInstance = this.addInfo(reviewEmailActivityId, prompt, answer);
       emailReviewTippyInstance.show();
     }
+
+    this.supplierMonitoring.highlightReviewEmail();
   }
 
   protected addInfo(activityId: string, prompt: string, answer: string) {
